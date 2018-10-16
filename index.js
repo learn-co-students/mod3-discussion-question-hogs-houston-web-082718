7b128afcb9a2ae3bb3c7cf6ed64e2987526e7dd4
Flatiron School <andrewsprague@outlook.com>
2018-10-16 10:01:31 -0500

    Done.

--- index.js
+++ index.js
@@ -27,24 +27,30 @@ function transitionPage(el, groupOut, groupIn) {
 
 function delayedFadeOut(div, range) {
   // Your solution here
-  fadeOut(div)
+  setTimeout(() => fadeOut(div), range);
 }
 
 function delayedFadeIn(div, range) {
   // Your solution here
-  fadeIn(div)
+  setTimeout(() => fadeIn(div), range);
+
 }
 
 function fadeAllOut(el, group) {
   // Your solution here
   group.forEach(div => {
-    delayedFadeOut(div)
+    if (div === el) {
+      delayedFadeOut(div, LONGRANGE)
+    } else {
+      delayedFadeOut(div, SHORTRANGE)
+    }
   })
 }
 
 function fadeAllIn(group) {
   // Your solution here
+  let randEl = group[Math.floor(Math.random() * group.length)];
   group.forEach(div => {
-    delayedFadeIn(div)
+    div === randEl ? delayedFadeIn(div, LONGRANGE) : delayedFadeIn(div, SHORTRANGE)
   })
 }
